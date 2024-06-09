20e9cb3539a5cd0af6ef788561e188e45cf2bccb
Michal Zarnecki <michal.zarnecki@companyhouse.de>
2024-06-09 22:04:16 +0200

    get documents from vector db based on similarity

--- app/src/process.php
+++ app/src/process.php
@@ -1,4 +1,8 @@
 <?php
+use Rajentrivedi\TokenizerX\TokenizerX;
+
+require __DIR__ . '/vendor/autoload.php';
+
 try {
     $conn = new PDO('pgsql:host=postgres;port=5432;dbname='.$_ENV["POSTGRES_DB"] .'',''.$_ENV["POSTGRES_USER"] .'',''.$_ENV["POSTGRES_PASSWORD"] .'');
 } catch (PDOException $e) {
@@ -14,18 +18,11 @@ if (! $prompt) {
     $prompt = 'What is the result of 2 + 2?';
 }
 
-use Rajentrivedi\TokenizerX\TokenizerX;
-
-require __DIR__ . '/vendor/autoload.php';
-
 const CONTEXT_TOKEN_COUNT = 1000;
 $apiKey = file_get_contents('api_key.txt');
 
 $client = OpenAI::Client($apiKey);
 $model = 'gpt-4o';
-
-$statement = $conn->prepare('CREATE TABLE IF NOT EXISTS document (id serial PRIMARY KEY, text text)');
-$statement->execute();
 
 #load documents
 $path    = 'documents';
@@ -37,17 +34,33 @@ foreach($files as $file) {
     $documents[] = $document;
 
     #load documents to postgres database
-    $statement = $conn->prepare("INSERT INTO document(text) VALUES(:doc)");
-    $statement->execute(array(
-        "doc" => $document
-    ));
+    $statement = $conn->prepare("INSERT INTO document(text, embedding) VALUES(:doc, :embed)");
+    $resp = $client->embeddings()->create([
+        'input' => $document,
+        'model' => 'text-embedding-ada-002'
+    ]);
+    $statement->execute([
+        "doc" => $document,
+        "embed" => json_encode($resp->embeddings[0]->embedding)
+    ]);
 }
+
+# get embeddings for prompt
+$respPrompt = $client->embeddings()->create([
+    'input' => $prompt,
+    'model' => 'text-embedding-ada-002'
+]);
+$embeddingPrompt =json_encode($resp->embeddings[0]->embedding);
+
+#find documents with similarity to prompt
+$documentsChosen = $conn->query("SELECT text from document order by embedding <-> '" . $embeddingPrompt . "'  desc limit 2;")
+    ->fetchAll();
 
 # prepare RAG input
 $contextTokenCount = CONTEXT_TOKEN_COUNT - TokenizerX::count($prompt) - 20;
 $input = '';
-foreach($documents as $document) {
-    $input .= $document . "\n";
+foreach($documentsChosen as $document) {
+    $input .= $document['text'] . "\n";
     $tokens = TokenizerX::tokens($input, "gpt-4");
 
     if (count($tokens) > $contextTokenCount) {
